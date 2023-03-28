ee73cafefcde709cca02d6ec7ca78c0c57aff486
MakenaG <makenaglory44@gmail.com>
2023-03-28 04:42:04 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/me', to: '/users#show'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/sessions/create', to: 'sessions#create'
+  get '/sessions/destroy', to: 'sessions#destroy'
+  get '/users/create', to: 'users#create'
+
 end
